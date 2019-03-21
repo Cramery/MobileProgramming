7af649b9f21e34340db73f61b2543bc9759f3583
Luca Rava <luca.rava@stud.hslu.ch>
2019-03-21 13:03:00 +0100

    first steps project receiver --> remove gradlew from project

---
+++ AndroidStudio/ServiceReveiver/app/src/main/java/com/example/servicereveiver/DemoMusicPlayerService.java
@@ -0,0 +1,29 @@
+package com.example.servicereveiver;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DemoMusicPlayerService extends Service{
+
+    @androidx.annotation.Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startPlayer();
+        return Service.START_NOT_STICKY;
+    }
+
+    private void startPlayer(){
+        if(playThread != null && playThread.isAlive()) return;
+        startPlayThread();
+        startForeground(NOTIFICATION_ID, createNotification("Playing..."));
+    }
+
+    @Override
+
+}
