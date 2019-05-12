8b98714169a92adfb9198bae23e929bcce797771
Cramery <rami-602@hotmail.com>
2019-05-12 15:49:09 +0200

    Registration

--- AndroidStudio/Foody/app/src/main/java/com/mobpro/foody/RegisterFragment.java
+++ AndroidStudio/Foody/app/src/main/java/com/mobpro/foody/RegisterFragment.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -43,6 +49,12 @@ public class RegisterFragment extends Fragment {
 
     public void Register(View v){
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        EditText eT_REmail = (EditText) getView().findViewById(R.id.eT_REmail);
+        EditText eT_RPassword = (EditText) getView().findViewById(R.id.eT_RPassword);
+
+        String email = eT_REmail.getText().toString();
+        String password = eT_RPassword.getText().toString();
+
         //Register
         
 
